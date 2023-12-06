d1dae23d022913d1260fa43f2c30597a46b4a80c
7654567 <khiblinsergiy@gmail.com>
2023-12-06 17:12:47 +0100

    lesson 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header />
+        <Navbar />
+        <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <img
+                src="https://media.istockphoto.com/id/1157442869/nl/foto/panoramisch-uitzicht-op-siebdniesjavrrie-lake-in-zweeds-lapland-vasterbotten-county-norrland.jpg?s=2048x2048&w=is&k=20&c=MhuM5Abs7Je29E6DeWhHlnsd9kYSUMKLm8YMOIh8THM="
+                alt="image"/>
+            <p>ava+description</p>
+            <p>my posts</p>
+            <p>new post</p>
+            <p>post 1</p>
+            <p>post 2</p>
+        </div>
+    );
+};
+
